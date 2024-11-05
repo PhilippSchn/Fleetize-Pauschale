61c2855d19c419bf816f82bf12d07fa288147d42
PhilippSchn <philippschneider47@gmail.com>
2024-11-05 19:38:24 +0100

    Initial Commit

---
+++ Fleetize Pauschale/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fleetize Pauschale
+//
+//  Created by Philipp Schneider on 05.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fleetize Pauschale/Fleetize_PauschaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleetize_PauschaleApp.swift
+//  Fleetize Pauschale
+//
+//  Created by Philipp Schneider on 05.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Fleetize_PauschaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
